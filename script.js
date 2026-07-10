efa1988ad2f2009be2c9a7b6ea8d2d621539be8a
Zakyarn <zakyamaharanii@gmail.com>
2026-07-11 01:43:11 +0700

    script.js

--- script.js
+++ script.js
@@ -401,7 +401,7 @@ flatpickr("#startDate", {
     dateFormat: "Y-m-d",
     minDate: "2019-01-01",
     maxDate: "2025-12-31",
-    defaultDate: "2019-01-01"
+    defaultDate: "2019-01-01",
     disableMobile: true,
     clickOpens: true
 });
